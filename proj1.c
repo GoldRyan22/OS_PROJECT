1a9a4f6904552d10b757dd15b4882d3c0507ecd1
Goldis Razvan <138977035+GoldRyan22@users.noreply.github.com>
2024-04-08 23:24:57 +0300

    recursive snapshot for project1.c

--- proj1.c
+++ proj1.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,7 +12,7 @@
 ssize_t WriteInSnap(struct stat folder_stat, int fout)
 {
 
-    ssize_t written =write(fout, &folder_stat.st_ino, sizeof(long int)); // inode
+    ssize_t written= write(fout, &folder_stat.st_ino, sizeof(long int)); // inode
     if (written==-1) return -4;
 
     written =write(fout, &folder_stat.st_dev, sizeof(long int)); // dev id
@@ -55,21 +57,95 @@ ssize_t WriteInSnap(struct stat folder_stat, int fout)
    return written;
 }
 
+int CheckDirOrFile(const char* path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    if(S_ISREG(path_stat.st_mode)) return 1; // return 1 if regular file
+    else
+    {
+        if(S_ISDIR(path.st_mode)) return 2;
+        else return -1;
+    }
+}
+
 int MakeSnap(char* pathname)
 {
+    int fin=open(pathname, O_RDONLY);
+    if(fin<0) 
+    {
+        perror("  could not open the folder");
+        return -1;
+    }
+
+    struct stat folder_stat;
     
+    if(stat(pathname, &folder_stat)<0)
+    {
+        close(fin);
+        perror("stat crashed");
+        return -2;
+    }
+
+    char* SnapName=malloc(sizeof(pathname)+5);
+    
+
+    //int fout=open("Snap.txt", O_RDWR | O_CREAT);
+    int fout=open(strcat(SnapName, ".txt"), O_RDWR | O_CREAT);
+    if(fout<0)
+    {
+        perror(" could not create the Snap.txt file");
+        return -3;
+    }
+
+    WriteInSnap(folder_stat, fout);
+
+    return 0;
+}
+
+int iterate_dir(char* pathname)
+{
+    struct dirent* dirent_pointer;
+    DIR* dir;
+
+
+    
+
+    if((dir=opendir(dirent_pointer))==NULL)
+    {
+        perror("could not open the directory");
+        return -1;
+    }
+
+    while((dirent_pointer=readdir(dir))!=NULL)
+    {
+        char* newPath;
+        strcpy(newPath, pathname); 
+        strcat(newPath,"/");
+        strcat(newPath, dirent_pointer.d_name);
+
+        int check=CheckDirOrFile(newPath);
+
+        if(check<=0) printf("not a reg file or directory");
+        else if(check==1) MakeSnap(newPath);
+        else if(check==2) iterate_dir(newPath);
+
+    }
 }
 
 int main(int argc, char* argv[])
 {
-
+    printf("Hello Word\n");
     
-
-
-
-    /*
     char* pathname=argv[1];
+
+    iterate_dir(pathname);
+
+
+
     
+    
+    /*
     int fin=open(pathname, O_RDONLY);
     if(fin<0) 
     {
